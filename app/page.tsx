3eb9e4e67c32a633eb1a5c79413cb9abb86c9bae
SaidHoja <saidhojabusiness@gmail.com>
2023-08-12 00:03:43 -0400

    mostly done, needs touches and project links

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import Hero from "../components/hero"
 import About from "../components/about"
 import Projects from "../components/projects"
+import Footer from "../components/footer"
 export default function Home() {
   return (
     <main className = "mx-auto max-w-3xl px-4 sn:px-4 md:max-w-5xl">
       <Hero/>
       <About/>
       <Projects/>
+      <Footer/>
     </main>
   )
 }
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,7 +1,6 @@
 "use client";
 import { Link } from "react-scroll/modules";
 import { useTheme } from "next-themes";
-import { RiMoonFill, RiSunFill } from "react-icons/ri";
 import { IoMdMenu, IoMdClose } from "react-icons/io";
 import "../styles/globals.css";
 import React, { useState } from "react";
@@ -24,11 +23,6 @@ const NAV_ITEMS: Array<NavItem> = [
     label: "Projects",
     page: "projects",
   },
-
-  {
-    label: "Experience",
-    page: "experience",
-  }
 ];
 
 export default function Navbar() {
@@ -36,13 +30,10 @@ export default function Navbar() {
   const currentTheme = theme === "system" ? systemTheme : theme;
   const [navbar, setNavbar] = useState(false);
   return (
-    <header className="w-full mx-auto px-4 bg-white shadow fixed top-0 z-50 dark:bg-stone-900 dark:border-b dark:border-stone-600">
-      <div className="justify-between md:items-center md:flex">
+    <nav className="w-full mx-auto px-4shadow fixed top-0 z-50 dark:bg-neutral-900 dark:border-b dark:border-stone-900 ">
+      <div className="justify-center md:items-center md:flex">
         <div>
           <div className="flex items-center justify-between py-3">
-            <div className="md:py-5 md:block">
-              <h2 className="text-2xl font-bold">Saidamir Umarkhodjaev</h2>
-            </div>
             <div className="md:hidden">
               <button onClick= {() => setNavbar(!navbar)}>
                 {navbar ? <IoMdClose size={30} /> : <IoMdMenu size={30} />}
@@ -54,13 +45,13 @@ export default function Navbar() {
           <div className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
               navbar ? "block" : "hidden"
             }`}  >
-          <div className=" items-center justify-center md:flex space-y-8 md:space-x-6 md:space-y-0">
+          <div className=" items-center justify-center md:flex space-y-8 md:space-x-6 md:space-y-0 text-xl md:h-[70px] h-auto pb-8 md:pb-0">
             {NAV_ITEMS.map((item, idx) => {
               return <Link 
               key={idx}
               to={item.page}
               className={
-                "block lg:inline-block text-neutral-900  hover:text-neutral-500 dark:text-neutral-100"
+                "block lg:inline-block text-neutral-900  hover:text-green-800 dark:text-neutral-100 hover:scale-110"
               }
               activeClass="active"
               spy={true}
@@ -70,28 +61,13 @@ export default function Navbar() {
               onClick={() => setNavbar(!navbar)
               }
               
-              >{item.label}
+              ><button>{item.label}</button>
               </Link>;
             })}
-            {currentTheme === "dark" ? (
-              <button
-                onClick={() => setTheme("light")}
-                className="bg-slate-100 p-2 rounded-xl"
-              >
-                <RiSunFill size={25} color="black" />
-              </button>
-            ) : (
-              <button
-                onClick={() => setTheme("dark")}
-                className="bg-slate-100 p-2 rounded-xl"
-              >
-                <RiMoonFill size={25} />
-              </button>
-            )}
             </div>
           </div>
         </div>
       </div>
-    </header>
+    </nav>
   );
 }
